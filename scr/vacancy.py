7317e81ea23cd8c823ca4c06da347bb576c64522
Sandi_Navoiy <santes.au@gmail.com>
2023-04-29 17:00:19 +0300

    реализация класса vacacy, начало

--- scr/vacancy.py
+++ scr/vacancy.py
@@ -2,23 +2,20 @@ from scr.abc import VacancyStorage
 
 
 class Vacancy(VacancyStorage):
-    def __init__(self, job_title: str,url_job, payment_range, requirements:str):
-        self.job_title = job_title  #описание проффесии запроса
-        self.url_job = url_job #урл запроса
-        self.payment_range = payment_range #уровень заработной платы(диапазон)
-        self.requirements = requirements  #требования
+    def __init__(self, job_title: str, url_job: str, payment_range, requirements: str):
+        if isinstance(job_title, str):
+            raise ValueError('Параметр "job_title" должен быть строкой')
+        self.job_title = job_title  # описание проффесии запроса
 
+        if isinstance(url_job, str):
+            raise ValueError('Параметр "url_job" должен быть строкой')
+        self.url_job = url_job  # урл запроса
 
-    def __init__(self):
-        pass
-    def add_vacancy(self, vacancy):
-        pass
+        if isinstance(payment_range, str):
+            raise ValueError('Параметр "payment_range" должен быть строкой')
+        self.payment_range = payment_range  # уровень заработной платы(диапазон)
 
-    def get_vacancies(self, **kwargs):
-        pass
-
-    def remove_vacancy(self, vacancy):
-        pass
-
-
+        if isinstance(requirements, str):
+            raise ValueError('Параметр "requirements" должен быть строкой')
+        self.requirements = requirements  # требования
 
